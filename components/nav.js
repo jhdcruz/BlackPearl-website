d89c0f8958e78e32628b88e0207e5005211a9c06
jhdcruz <jhdcruz00@gmail.com>
2019-12-18 09:36:21 +0800

    Added state to static navbar | Hamburger Menu working

--- components/nav.js
+++ components/nav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Navbar from "react-bulma-components/lib/components/navbar";
 import { createGlobalStyle } from "styled-components";
@@ -33,30 +33,51 @@ const links = [
   return link;
 });
 
-const Nav = () => (
-  <Navbar transparent={true}>
-    <NavStyle />
-    <Navbar.Brand>
-      <Navbar.Item renderAs="a" href="/">
-        <img
-          src="/brand.svg"
-          alt="BlackPearl | Home of the Bubble Tea Shop in Bacoor"
-          width="auto"
-          height="auto"
-        />
-      </Navbar.Item>
-      <Navbar.Burger />
-    </Navbar.Brand>
-    <Navbar.Menu>
-      <Navbar.Container position="end">
-        {links.map(({ key, href, label }) => (
-          <Navbar.Item href={href} key={key}>
-            {label}
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+
+    // TODO: Received `false` for a non-boolean attribute `active`.
+    // TODO: Failed prop type: Invalid prop `active` of type `string` supplied to `Navbar`, expected `boolean`.
+    // Set active state for menu
+    this.state = { active: false };
+    // Active state manager for menu
+    this.handleClick = () => {
+      const { active } = this.state;
+      this.setState({ active: !active });
+    };
+  }
+
+  render() {
+    return (
+      <Navbar transparent={true} active={this.state.active}>
+        <NavStyle />
+        <Navbar.Brand>
+          <Navbar.Item renderAs="a" href="/">
+            <img
+              src="/brand.svg"
+              alt="BlackPearl | Home of the Bubble Tea Shop in Bacoor"
+              width="auto"
+              height="auto"
+            />
           </Navbar.Item>
-        ))}
-      </Navbar.Container>
-    </Navbar.Menu>
-  </Navbar>
-);
+          <Navbar.Burger
+            active={this.state.active}
+            onClick={this.handleClick}
+          />
+        </Navbar.Brand>
+        <Navbar.Menu>
+          <Navbar.Container position="end">
+            {links.map(({ key, href, label }) => (
+              <Navbar.Item href={href} key={key}>
+                {label}
+              </Navbar.Item>
+            ))}
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Navbar>
+    );
+  }
+}
 
 export default Nav;
