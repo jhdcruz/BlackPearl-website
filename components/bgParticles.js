a68d6ab2980a3ccbf667c87fe7794617669d8fb1
jhdcruz <jhdcruz00@gmail.com>
2019-12-18 10:24:02 +0800

    react-particle-background dependency & impl.

---
+++ components/bgParticles.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import Particles from "react-particles-js";
+import { createGlobalStyle } from "styled-components";
+import particlesData from "./data/particlesjs-config";
+
+const ParticleTheme = createGlobalStyle`
+canvas,
+  .particle-bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0.85;
+    overflow: hidden !important;
+    z-index: 0;
+  }
+`;
+
+class BgParticles extends Component {
+  render() {
+    return <Particles params={particlesData} canvasClassName="particle-bg" />;
+  }
+}
+
+export default BgParticles;
