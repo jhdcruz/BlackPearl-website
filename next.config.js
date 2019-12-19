a0429720ddecc5a2315803c4f54130e3ba31e794
jhdcruz <jhdcruz00@gmail.com>
2019-12-19 21:16:42 +0800

    fixed module exporting as a singular export

--- next.config.js
+++ next.config.js
@@ -2,26 +2,23 @@ const withCss = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
 const withTM = require("next-transpile-modules");
 
-// TODO: Exporting
-module.exports = {
-  exportPathMap: async function() {
-    return {
-      "/": { page: "/" },
-      "/menu": { page: "/menu" },
-      "/about": { page: "/about" }
-    };
-  }
-};
-
-module.exports = withCss({
-  cssModules: true
-});
-
-module.exports = withSass(
-  withTM({
-    transpileModules: ["react-bulma-components"],
-    sassLoaderOptions: {
-      includePaths: ["./components"]
-    }
-  })
+module.exports = withCss(
+  withSass(
+    withTM({
+      transpileModules: ["react-bulma-components"],
+      sassLoaderOptions: {
+        includePaths: ["./components"]
+      },
+      exportPathMap: async function(
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+      ) {
+        return {
+          "/": { page: "/" },
+          "/menu": { page: "/menu" },
+          "/about": { page: "/about" }
+        };
+      }
+    })
+  )
 );
