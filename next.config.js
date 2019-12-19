026ca85e2fd5cda51944b38eb84e436418568e5f
Aegir Aideron <jhdcruz00@gmail.com>
2019-12-19 20:46:17 +0800

    static export [WIP]

--- next.config.js
+++ next.config.js
@@ -2,9 +2,21 @@ const withCss = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
 const withTM = require("next-transpile-modules");
 
+// TODO: Exporting
+module.exports = {
+  exportPathMap: async function() {
+    return {
+      "/": { page: "/" },
+      "/menu": { page: "/menu" },
+      "/about": { page: "/about" }
+    };
+  }
+};
+
 module.exports = withCss({
   cssModules: true
 });
+
 module.exports = withSass(
   withTM({
     transpileModules: ["react-bulma-components"],
