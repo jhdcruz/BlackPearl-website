cd1166ee2c409418abe8dc84137fe660ebdb80e8
jhdcruz <jhdcruz00@gmail.com>
2020-01-09 14:23:54 +0800

    Fix scrollbar functionality while hidden

--- src/index.js
+++ src/index.js
@@ -23,7 +23,10 @@ const GlobalStyle = createGlobalStyle`
     font-family: "Kanit", "Segoe UI", sans-serif;
     background-color: #323232;
     overflow-x: hidden !important;
-    overflow-y: hidden !important;
+  }
+
+  ::-webkit-scrollbar {
+    display: none;
   }
 
   .hero {
--- src/layouts/About.js
+++ src/layouts/About.js
@@ -2,5 +2,10 @@ import React from "react";
 import "dependency/@fortawesome/fontawesome-free/css/all.min.css";
 
 export default function About() {
-  return <div className="container" />;
+  return (
+    <div className="columns">
+      <div className="column"></div>
+      <div className="column"></div>
+    </div>
+  );
 }
