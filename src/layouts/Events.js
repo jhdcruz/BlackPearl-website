a2408d6164e14cf0998d86a795ff9fc81e398880
jhdcruz <jhdcruz00@gmail.com>
2020-02-04 22:12:59 +0800

    adjusted font size

--- src/layouts/Events.js
+++ src/layouts/Events.js
@@ -3,12 +3,12 @@ import styled from "styled-components";
 
 const Notice = styled.h4`
   color: rgba(255, 128, 162, 1) !important;
-  font-size: 3rem;
+  font-size: 2rem;
   font-weight: bold;
   text-align: center;
   text-transform: uppercase;
   text-shadow: -1px 3px 2px #323232;
-  margin: 12rem auto 0 auto;
+  margin: 13rem auto 0 auto;
   line-height: 1.1;
   display: block;
 `;
