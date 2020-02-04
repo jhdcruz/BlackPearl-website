9a139010e49dcbade6cc93ee9cf1f9d499e28570
jhdcruz <jhdcruz00@gmail.com>
2020-02-04 23:02:34 +0800

    Partial Testimonial UI update

--- src/components/testimonialContent.js
+++ src/components/testimonialContent.js
@@ -1,11 +1,50 @@
 import React from "react";
 import Testimonials from "./data/testimonials.data.json";
+import styled from "styled-components";
 
 /*
  * ===================
  * Components Style
  * ===================
  */
+
+const Media = styled.div`
+  margin: 10px auto !important;
+`;
+
+const Profile = styled.img`
+  border-radius: 50%;
+`;
+
+const Recommendation = styled.p`
+  color: rgba(255, 128, 162, 1) !important;
+  text-shadow: -1px 3px 2px #323232;
+
+  span {
+    color: #ff4c93;
+  }
+`;
+
+const ReviewCard = styled.div`
+  background-color: rgba(20, 20, 20, 0.6);
+`;
+
+const Reviewer = styled.p`
+  color: rgba(255, 128, 162, 1) !important;
+  text-shadow: -1px 3px 2px #323232;
+`;
+
+const ReviewContent = styled.div`
+  color: rgba(255, 128, 162, 1) !important;
+  text-shadow: -1px 3px 2px #323232;
+  padding: 0;
+`;
+
+const ReviewDate = styled.h6`
+  color: #ff4c93 !important;
+  text-shadow: -1px 3px 2px #323232;
+  margin: 10px auto auto auto;
+`;
 
 /*
  * ===================
@@ -14,32 +53,33 @@ import Testimonials from "./data/testimonials.data.json";
  */
 export default function TestimonialContent() {
   return (
-    <div className="card">
+    <ReviewCard className="card">
       <div className="card-content">
         {Testimonials.testimonial.map(review => (
           <div key={review.id}>
-            <div className="media">
+            <Media className="media">
               <div className="media-left">
                 <figure className="image is-48x48">
-                  <img src={review.profile} alt="Placeholder image" />
+                  {/* TODO: Replace with local image | to be presented offline! */}
+                  <Profile src={review.profile} alt="Placeholder image" />
                 </figure>
               </div>
               <div className="media-content">
-                <p className="title is-4">{review.name}</p>
-                <p className="subtitle is-6">
+                <Reviewer className="title is-4">{review.name}</Reviewer>
+                <Recommendation className="subtitle is-6">
                   recommends <span>BlackPearl in your area</span>.
-                </p>
+                </Recommendation>
               </div>
-            </div>
+            </Media>
 
-            <div className="content">
+            <ReviewContent className="content">
               {review.content}
               <br />
-              <p>{review.datetime}</p>
-            </div>
+              <ReviewDate>{review.datetime}</ReviewDate>
+            </ReviewContent>
           </div>
         ))}
       </div>
-    </div>
+    </ReviewCard>
   );
 }
