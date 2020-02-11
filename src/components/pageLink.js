12b45c51a996b3146fbe48301063cca6eff7f6cc
Aegir Aideron <jhdcruz00@gmail.com>
2020-02-11 21:50:43 +0800

    Fixed unaligned fa-icons

--- src/components/pageLink.js
+++ src/components/pageLink.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
 
 /*
  * =====================
  * React-router's Link
  * =====================
  */
+const ButtonLink = styled(NavLink)`
+  padding: 3px 13px !important;
+`;
+
 export default function PageLink(props) {
   return (
-    <NavLink to={props.page} className={props.theme}>
+    <ButtonLink to={props.page} className={props.theme}>
       <i className={props.icon} /> {props.name}
-    </NavLink>
+    </ButtonLink>
   );
 }
